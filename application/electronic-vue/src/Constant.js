2bcb8015766ca9a76431e1583bd9ee4a7cd53715
Muppalla Surya <suryamuppalla@gmail.com>
2021-06-08 16:40:25 +0530

    Changes

--- application/electronic-vue/src/Constant.js
+++ application/electronic-vue/src/Constant.js
@@ -1,4 +1,4 @@
 export class Constant {
-    static API_URL = `http://localhost/2004138/electronic/index.php/api`;
-    static IMG_URL = `http://localhost/2004138/electronic/upload/`;
+    static API_URL = `https://mi-linux.wlv.ac.uk/~2004138/electronic/index.php/api`;
+    static IMG_URL = `https://mi-linux.wlv.ac.uk/~2004138/electronic/upload/`;
 }
