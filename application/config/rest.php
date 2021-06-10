152e0c41fd423b96ab8d68471cb53e55e5cec9d1
Muppalla Surya <suryamuppalla@gmail.com>
2021-06-10 06:44:30 +0530

    Changes

--- application/config/rest.php
+++ application/config/rest.php
@@ -604,7 +604,7 @@ $config['allow_any_cors_domain'] = TRUE;
 | e.g. $config['allowed_origins'] = ['http://www.example.com', 'https://spa.example.com']
 |
 */
-$config['allowed_cors_origins'] = ['https://mi-linux.wlv.ac.uk/~2004138/electronic/public'];
+$config['allowed_cors_origins'] = ['https://mi-linux.wlv.ac.uk/~2004138/electronic/public', 'http://localhost:8080'];
 
 /*
 |--------------------------------------------------------------------------
